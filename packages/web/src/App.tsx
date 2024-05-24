fdeb36fcfb5d56424deab2e33bc874990e04600d
Brandon Phillips <brandon.phillips@bphillips.dev>
2024-05-24 14:05:31 -0700

    WEB

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,6 +5,8 @@ import Navigation from "./components/Navigation/Navigation";
 import Notifications from "./components/Notifications/Notifications";
 import MainNav from "./components/MainNav/MainNav";
 
+console.log("hi");
+
 function App() {
   useResponsiveSetup();
 
