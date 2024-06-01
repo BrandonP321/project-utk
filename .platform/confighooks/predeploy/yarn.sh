2bcb1e23dfba9ba1570c7b3e5c18461b346a45da
Brandon Phillips <brandon.phillips@bphillips.dev>
2024-06-01 00:00:16 -0700

    Log into codeartifact in elastic beanstalk predeploy scripts

--- .platform/confighooks/predeploy/yarn.sh
+++ .platform/confighooks/predeploy/yarn.sh
@@ -4,8 +4,8 @@ npm i -g corepack # Required if using Amazon Linux 2023
 
 echo "Enable corepack"
 corepack enable # Enable Corepack
-# echo "Set AWS CodeArtifact token"
-# .  bin/set-artifact-token.sh
-# echo "Run yarn install"
+echo "Set AWS CodeArtifact token"
+.  bin/set-artifact-token.sh
+echo "Run yarn install"
 # yarn install
 # echo "Yarn install finished"
