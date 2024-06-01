e6bf0e0f44c2053dd1aaa211ff2dc1fa23199ccc
Brandon Phillips <brandon.phillips@bphillips.dev>
2024-05-31 19:00:06 -0700

    Add enhanced logging to elastic beanstalk hooks

--- .platform/hooks/predeploy/yarn.sh
+++ .platform/hooks/predeploy/yarn.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Install corepack"
 npm i -g corepack # Required if using Amazon Linux 2023
 
+echo "Enable corepack"
 corepack enable # Enable Corepack
+echo "Set AWS CodeArtifact token"
 .  bin/set-artifact-token.sh
+echo "Run yarn install"
 yarn install
+echo "Yarn install finished"
