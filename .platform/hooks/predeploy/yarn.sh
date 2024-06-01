6a3959afbf46a7fc9a14f8445a1899d2a82d97b7
Brandon Phillips <brandon.phillips@bphillips.dev>
2024-05-31 23:54:51 -0700

    Enable corepack in elastic beanstalk

--- .platform/hooks/predeploy/yarn.sh
+++ .platform/hooks/predeploy/yarn.sh
@@ -2,8 +2,8 @@
 echo "Install corepack"
 npm i -g corepack # Required if using Amazon Linux 2023
 
-# echo "Enable corepack"
-# corepack enable # Enable Corepack
+echo "Enable corepack"
+corepack enable # Enable Corepack
 # echo "Set AWS CodeArtifact token"
 # .  bin/set-artifact-token.sh
 # echo "Run yarn install"
